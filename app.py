50fa53d917920fe8a5a26ba8ea16f7d69a04f45f
Aukksi <112703170+Aukksi@users.noreply.github.com>
2023-12-21 17:05:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+   print('Moro')
+   return 'Tere'
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/hello', methods=['GET'])
 def hello():
