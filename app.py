c4194b2506b93594a1c99456142699b299ae61a7
Aukksi <112703170+Aukksi@users.noreply.github.com>
2023-12-21 17:11:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   print('Moro')
+   #print('Moro')
    return 'Tere'
 
 
-@app.route('/hello', methods=['GET'])
-def hello():
-  os.system(f'python {/paivansoppa.py}')
+#@app.route('/hello', methods=['GET'])
+#def hello():
+#  os.system(f'python {/paivansoppa.py}')
 
 if __name__ == '__main__':
    app.run()
